abfb44cc6775b19b145c5dc1a6b6b42dd82430b7
curiousbull <curiousbull@163.com>
2017-03-09 15:36:42 +0800

    March 9, afternoon

---
+++ ipython_log.py
@@ -0,0 +1,10 @@
+# IPython log file
+
+get_ipython().system('date')
+a = 2+2
+a
+get_ipython().magic('hist')
+get_ipython().magic('logstart')
+get_ipython().system('ls ipython_log.py')
+get_ipython().system('cat ipython_log.py')
+get_ipython().magic('logoff')
